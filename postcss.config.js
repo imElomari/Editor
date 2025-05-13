1269cb0aa47420281f3f602c5329c9029b5d3682
Dev <dev@atlasimex.es>
2025-05-13 03:02:42 +0200

    fix postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,4 @@
-import tailwindcss from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
-
-/** @type {import('postcss').ProcessOptions} */
-export default {
-  plugins: [tailwindcss, autoprefixer],
-}
+export const plugins = {
+  '@tailwindcss/postcss': {},
+  'autoprefixer': {},
+};
